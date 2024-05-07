2ea38b623b53cf0eaf8f4e78d23445b475a60866
Erik Trejo <erik.trejob@gmail.com>
2024-05-07 09:52:32 -0600

    primer commit de la app países, fin de la sección

---
+++ src/app/shared/components/loaderSpinner/loaderSpinner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-loader-spinner',
+  templateUrl: './loaderSpinner.component.html',
+  styleUrls: ['./loaderSpinner.component.css'],
+})
+export class LoaderSpinnerComponent { }
